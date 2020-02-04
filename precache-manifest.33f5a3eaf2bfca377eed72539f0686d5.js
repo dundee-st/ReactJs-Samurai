5af875ee006d95f5daced23405c920753c329426
dundee-st <dundee-st@users.noreply.github.com>
2020-02-05 01:29:29 +0200

    Updates

---
+++ precache-manifest.33f5a3eaf2bfca377eed72539f0686d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff64edd9032eee7d0f9c1c090db4f5c",
+    "url": "/ReactJs-Samurai/index.html"
+  },
+  {
+    "revision": "7fe17638ee35bba76bdd",
+    "url": "/ReactJs-Samurai/static/css/3.749cb2db.chunk.css"
+  },
+  {
+    "revision": "502d685b0e180a2209bc",
+    "url": "/ReactJs-Samurai/static/css/4.029ef923.chunk.css"
+  },
+  {
+    "revision": "3a117a8f5042db08e41e",
+    "url": "/ReactJs-Samurai/static/css/main.b0adf152.chunk.css"
+  },
+  {
+    "revision": "99d62fde5c191cf8920c",
+    "url": "/ReactJs-Samurai/static/js/2.35ffd562.chunk.js"
+  },
+  {
+    "revision": "7fe17638ee35bba76bdd",
+    "url": "/ReactJs-Samurai/static/js/3.900febbb.chunk.js"
+  },
+  {
+    "revision": "502d685b0e180a2209bc",
+    "url": "/ReactJs-Samurai/static/js/4.56211f37.chunk.js"
+  },
+  {
+    "revision": "3a117a8f5042db08e41e",
+    "url": "/ReactJs-Samurai/static/js/main.f64c2fbb.chunk.js"
+  },
+  {
+    "revision": "d6503e0dd4e3af718dc4",
+    "url": "/ReactJs-Samurai/static/js/runtime-main.f06ba965.js"
+  },
+  {
+    "revision": "4cb635a9cd9012974ca4a58a8b7ee344",
+    "url": "/ReactJs-Samurai/static/media/Spinner-1s-200px.4cb635a9.svg"
+  }
+]);
