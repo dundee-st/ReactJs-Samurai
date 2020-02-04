280a436bc31aeda948ebd9573addaa1879a25707
dundee-st <dundee-st@users.noreply.github.com>
2020-02-05 01:11:56 +0200

    del

--- src/components/MusicComp/MusicComp.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// import classes from './Dialogs.module.css';
-
-const MusicComp = () => {
-    return <div>Music</div>;
-};
-export default MusicComp;
--- src/components/News/News.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// import classes from './Dialogs.module.css';
-
-const News = () => {
-    return <div>News</div>;
-};
-export default News;
